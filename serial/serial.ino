57de4c697085402ead28cef7ca4875a8c44287f4
Vicky Rig <vicky@o1.com>
2021-08-26 17:39:47 +0200

    Add arduino code for TM horn

--- serial/serial.ino
+++ serial/serial.ino
@@ -2,6 +2,7 @@
 String serialRxString;
 char serialRxBytes[3];
 int readUntilCode;
+bool hornOn;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,24 +15,30 @@ void setup() {
   {
     pinMode(i, OUTPUT);
   }
+  pinMode(A2, OUTPUT);
+  hornOn = false;
 }     
 
 void loop() {
+  
     if (Serial.available() > 0) { 
       memset(serialRxBytes, 0 , sizeof(serialRxBytes));
       readUntilCode = Serial.readBytesUntil(';', serialRxBytes, 3);
-      // serialRxBytes[0] = serialRxBytes[0] - '0';
-      // serialRxBytes[1] = serialRxBytes[0] - '0';
-      /*Serial.print(serialRxBytes);
-      Serial.print(" PLATS 0: ");
-      Serial.print(serialRxBytes[0]);
-      Serial.print(" PLATS 1: ");
-      Serial.print(serialRxBytes[1]);
-      Serial.print("\n");*/
   
       if ( serialRxBytes[0] >= 0 && serialRxBytes[0] <= 13 && serialRxBytes[1] >= 0 && serialRxBytes[1] <= 1) {
         // Serial.print("WRITING TO PIN:");Serial.print(serialRxBytes[0]);Serial.print(" STATE: ");Serial.print(serialRxBytes[1]);
         digitalWrite(serialRxBytes[0], serialRxBytes[1]);
       }
     }
+
+    // A0 Is connected to TM horn adn TM gnd
+    if (analogRead(A0) < 10) {
+      hornOn = true;
+      digitalWrite(A2, 1);
+    }
+    else if (hornOn) {
+      digitalWrite(A2, 0);
+      hornOn = false;
+    }
+    Serial.println(hornOn);
 }
